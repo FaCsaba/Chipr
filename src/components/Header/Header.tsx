d994978ff50a001a9abcd127fe8da0f0cdc760a9
Raq-git <csab.faz.2@gmail.com>
2022-05-12 18:05:47 +0200

    Header now correctly shows what page you're on

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,40 @@
 import Classes from './Header.module.css';
 import {GiHamburgerMenu} from 'react-icons/gi';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 import ChirprContext from '../../store/ChirprContext';
 
 
 function Header() { // TODO: after we set up states; only show prompt when logged in; change Link to /user+user_id 
     const {isLoggedIn} = useContext(ChirprContext);
-
+    const {pathname} = useLocation()
 
 
     return <>
         <header className={Classes.Header}>
             <Link to={'/'} className={Classes.NavigationButton} >
-                <p className={Classes.Primary}>Home</p>
+                <p className={pathname === '/' ? Classes.Primary : ''}>Home</p> 
             </Link>
-            {isLoggedIn &&
+            {isLoggedIn?
                 <>
                     <Link to={'/user'} className={Classes.NavigationButton}>
-                        <p >Profile</p>
+                        <p className={pathname.startsWith('/user') ? Classes.Primary : ''}>Profile</p>
                     </Link>
                     <Link to={'/settings'} className={Classes.NavigationButton}>
-                        <p>Settings</p>
+                        <p className={pathname.startsWith('/settings') ? Classes.Primary : ''}>Settings</p>
                     </Link>
                 </>
+                : 
+                <>
+                    <Link to={'/login'} className={Classes.NavigationButton}>
+                        <p className={pathname.startsWith('/login') ? Classes.Primary : ''}>Login</p>
+                    </Link>
+                    <Link to={'/register'} className={Classes.NavigationButton}>
+                        <p className={pathname.startsWith('/register') ? Classes.Primary : ''}>Register</p>
+                    </Link>
+                </>
+                
             }
-            <Link to={'/login'} className={Classes.NavigationButton}>
-                <p>Login</p>
-            </Link>
-            <Link to={'/register'} className={Classes.NavigationButton}>
-                <p>Register</p>
-            </Link>
             <div className={Classes.HamburgerMenu}>
                 <GiHamburgerMenu/>     
             </div>
